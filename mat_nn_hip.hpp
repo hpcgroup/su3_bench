c9cbfb814f7708c2dbbaa556c381d27cf41b74a2
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-09-10 11:14:21 -0400

    feat: add aligned timers for hip model

--- mat_nn_hip.hpp
+++ mat_nn_hip.hpp
@@ -78,6 +78,10 @@ double su3_mat_nn(std::vector<site> &a, std::vector<su3_matrix> &b, std::vector<
     printf("Using device %d: %s\n", use_device, device_prop.name);
   }
 
+#ifdef ALIGNED_WORK
+  auto tstart = Clock::now();
+#endif
+
   // Declare target storage and copy A and B
   hipError_t cuErr;
   site *d_a, *d_c;
@@ -100,7 +104,9 @@ double su3_mat_nn(std::vector<site> &a, std::vector<su3_matrix> &b, std::vector<
   }
 
   // benchmark loop
+#ifndef ALIGNED_WORK
   auto tstart = Clock::now();
+#endif
   for (int iters=0; iters<iterations+warmups; ++iters) {
     if (iters == warmups) {
       hipDeviceSynchronize();
