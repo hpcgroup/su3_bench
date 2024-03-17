95858dd32ea97f2457aea384fe913ea941570837
Pranav Sivaraman <psivaram@umd.edu>
2024-03-17 13:24:20 -0400

    feat(HIP): use similiar loop structure

--- mat_nn_hip.hpp
+++ mat_nn_hip.hpp
@@ -13,30 +13,20 @@
 //  void mult_su3_nn( su3_matrix *a,*b,*c )
 //  matrix multiply, no adjoints 
 //  C  <-  A*B	
-__global__ void k_mat_nn(
-  const site*       __restrict__ a,
-  const su3_matrix* __restrict__ b,
-        site*       __restrict__ c,
-  int               total_sites)
-{
-  int myThread = blockDim.x * blockIdx.x + threadIdx.x;
-  int mySite = myThread/36;
 
-  if (mySite < total_sites) {
-    int j = (myThread%36)/9;
-    int k = (myThread%9)/3;
-    int l = myThread%3;
-    Complx cc = {0.0,0.0};
-    for (int m=0;m<3;m++)
-#ifdef MILC_COMPLEX
-      CMULSUM(a[mySite].link[j].e[k][m], b[j].e[m][l], cc);
-    //c[mySite].link[j].e[k][l].real = cc.real;
-    //c[mySite].link[j].e[k][l].imag = cc.imag;
-#else
-      cc += a[mySite].link[j].e[k][m] * b[j].e[m][l];
-    //c[mySite].link[j].e[k][l] = cc;
-#endif
-    c[mySite].link[j].e[k][l] = cc;
+__global__ void k_mat_nn(const site *a, const su3_matrix *b, site *c, int total_sites) {
+  for (int i = blockIdx.x; i < total_sites; i++) {
+    for (int j = threadIdx.x; j < 4; j++) {
+      for (int k = threadIdx.y; k < 3; k++) {
+        for (int l = threadIdx.z; l < 3; l++) {
+          Complx cc = {0.0, 0.0};
+          for (int m = 0; m < 3; m++) {
+            cc += a[i].link[j].e[k][m] * b[j].e[m][l];
+          }
+          c[i].link[j].e[k][l] = cc; 
+        }
+      }
+    }
   }
 }
 
