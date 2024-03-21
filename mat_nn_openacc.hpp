5d4716fa0a4621fb3dcb711828c2bcf1ac4496f5
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2024-03-21 08:12:08 -0700

    feat(ACC): Tuning OpenACC application

--- mat_nn_openacc.hpp
+++ mat_nn_openacc.hpp
@@ -1,63 +1,44 @@
 // OpenACC implementation
 
 double su3_mat_nn(std::vector<site> &a, std::vector<su3_matrix> &b, std::vector<site> &c, 
-		  size_t total_sites, size_t iterations, size_t threads_per_team, int use_device, Profile* profile)
-{
-  site *d_a, *d_c;
-  su3_matrix *d_b;
-  size_t len_a, len_b, len_c;
-  d_a = a.data(); len_a = a.size();
-  d_b = b.data(); len_b = b.size();
-  d_c = c.data(); len_c = c.size();
+		  size_t total_sites, size_t iterations, size_t threads_per_team, int use_device, Profile* profile) {
+  site * __restrict__ d_a = std::data(a);
+  su3_matrix * __restrict__ d_b = std::data(b);
+  site * __restrict__ d_c = std::data(c);
+
+  size_t size_a = std::size(a);
+  size_t size_b = std::size(b);
+  size_t size_c = std::size(c);
 
   auto tprofiling = Clock::now();
 
-  // Move A, B and C vectors to the device
-  #pragma acc enter data copyin(d_a[0:len_a], d_b[0:len_b], d_c[0:len_c])
-
-  profile->host_to_device_time = (std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tprofiling).count())/1.0e6;
-
-  // benchmark loop
   auto tstart = Clock::now();
-  tprofiling = tstart;
-  for (int iters=0; iters<iterations+warmups; ++iters) {
+
+  #pragma acc data copyin(d_a[:size_a], d_b[:size_b]) copyout(d_c[:size_c])
+  for (size_t iters = 0; iters < iterations + warmups; iters++) {
     if (iters == warmups) {
       tstart = Clock::now();
       tprofiling = tstart;
     }
-    #pragma acc parallel loop gang present(d_a[0:len_a], d_b[0:len_b], d_c[0:len_c])
-    for(int i=0;i<total_sites;++i) {
-      #pragma acc loop worker vector collapse(3)
-      for (int j=0; j<4; ++j) {
-        for(int k=0;k<3;k++) {
-          for(int l=0;l<3;l++){
-	    Complx cc = {0.0, 0.0};
-#ifndef MILC_COMPLEX
-            #pragma acc loop seq
-            for(int m=0;m<3;m++) {
-               cc += d_a[i].link[j].e[k][m] * d_b[j].e[m][l];
+
+    #pragma acc parallel loop collapse(4)
+    for (size_t i = 0; i < total_sites; i++) {
+      for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 3; k++) {
+          for (int l = 0; l < 3; l++) {
+            Complx cc{};
+            for (int m = 0; m < 3; m++) {
+              cc += d_a[i].link[j].e[k][m] * d_b[j].e[m][l];
             }
             d_c[i].link[j].e[k][l] = cc;
-#else
-            #pragma acc loop seq
-            for(int m=0;m<3;m++) {
-               CMULSUM(d_a[i].link[j].e[k][m], d_b[j].e[m][l], cc);
-            }
-            d_c[i].link[j].e[k][l].real = cc.real;
-            d_c[i].link[j].e[k][l].imag = cc.imag;
-#endif
           }
         }
       }
     }
   }
-  double ttotal = std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tstart).count();
+
   profile->kernel_time = (std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tprofiling).count())/1.0e6;
+  double ttotal = std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tstart).count();
 
-  // move the result back
-  tprofiling = Clock::now();
-  #pragma acc exit data copyout(d_c[0:len_c])
-  profile->device_to_host_time = (std::chrono::duration_cast<std::chrono::microseconds>(Clock::now()-tprofiling).count())/1.0e6;
-
-  return (ttotal /= 1.0e6);
+  return ttotal;
 }
